f627af9847bd100bf9ca4e34e4c8436daad9b0ac
ThaboKalima <71952387+ThaboKalima@users.noreply.github.com>
2026-06-16 16:01:25 +0100

    add tf for app

--- app-deployment/tf-app-instance/main.tf
+++ app-deployment/tf-app-instance/main.tf
@@ -5,30 +5,32 @@ provider "aws" {
     region = var.default_region
 }
 
-# Makes get request to that url
-data "http" "my_ipv4" {
-  url = "https://ipv4.icanhazip.com"
-}
 
 resource "aws_security_group" "app_sg"{
     name = var.security_group_name
     description = var.security_group_name
 
     ingress {
-        from_port = var.port_1
-        to_port = var.port_1
+        from_port = var.ssh_port
+        to_port = var.ssh_port
         protocol = "tcp"
-        cidr_blocks =   ["${chomp(data.http.my_ipv4.response_body)}/32"]
+        cidr_blocks =   [var.source_cidr_blocks]
     }
     ingress {
-        from_port = var.port_2
-        to_port = var.port_2
+        from_port = var.http_port
+        to_port = var.http_port
         protocol = "tcp" 
         cidr_blocks = [var.source_cidr_blocks]
     }
     ingress {
-        from_port = var.port_4
-        to_port = var.port_4
+        from_port = var.grafana_port
+        to_port = var.grafana_port
+        protocol = "tcp" 
+        cidr_blocks = [var.source_cidr_blocks]
+    }
+     ingress {
+        from_port = var.prometheus_port
+        to_port = var.prometheus_port
         protocol = "tcp" 
         cidr_blocks = [var.source_cidr_blocks]
     }
@@ -40,27 +42,22 @@ resource "aws_security_group" "app_sg"{
     }
 }
 #create  ec2 instance
-resource "aws_instance" "test_instance" {
+resource "aws_instance" "app_instance" {
     # specify AMI id (Ubuntu 24.04 LTS)
     ami = var.ubuntu24_ami_id
     instance_type = var.instance_size
     associate_public_ip_address = true
     subnet_id = var.default_public_subnet_id 
     key_name = var.my_key_pair
-    #user_data = file("${path.module}/${var.user_data_file}")
     vpc_security_group_ids  = [aws_security_group.app_sg.id]
-   
-    root_block_device {
-    volume_size           = var.disk_size 
-    volume_type           = "gp3"
-    delete_on_termination = true
-    }
+    #user_data = file("${path.module}/user-data.sh") if/when use user data
     tags = {
         Name = var.instance_name_tag
     }
 
 }
+# gives static ip from Ramon's jenkins server 2
 resource "aws_eip_association" "static_ip_assoc" {
-  instance_id   = aws_instance.test_instance.id 
-  allocation_id = "eipalloc-0d1dbe0e01eb6f223" 
+  instance_id   = aws_instance.app_instance.id 
+  allocation_id = "eipalloc-04af77e9998fba023" 
 }
